bb9e019de2cb5cae1d47b087585c17a10eb1c5e2
Leon Mwandiringa <leonmwandiringa@gmail.com>
2024-05-05 22:25:50 +0200

    zone

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "arn" {
   value = aws_cloudfront_distribution.default.arn
   description = "distribution arn"
 }
+
+output "hosted_zone_id" {
+  value = aws_cloudfront_distribution.default.hosted_zone_id
+  description = "distribution arn"
+}
